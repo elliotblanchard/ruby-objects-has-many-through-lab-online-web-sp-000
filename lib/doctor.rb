fecb7363235d840b26180a8c890465f4fe9af763
Elliot Blanchard <public@elliotblanchard.com>
2020-04-20 01:33:49 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date,patient)
+    appointment.new(date,patient,self)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
